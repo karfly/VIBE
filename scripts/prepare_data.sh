f8d78012eadf0d086a9f01dc9fea08dbfedf3e44
Karim Iskakov <karfly@yandex.ru>
2020-08-06 18:35:29 +0300

    Update prepare_data.sh
    +docker

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export CONDA_ENV_NAME=vibe-env
+eval "$(conda shell.bash hook)"
+conda activate $CONDA_ENV_NAME
+
 mkdir -p data
 cd data
 gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
